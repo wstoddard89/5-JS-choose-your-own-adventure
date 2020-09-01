e01e3987dff6baba97bfeb3c81a76fa3bf6605e4
Will Stoddard <wstoddard89@gmail.com>
2020-09-01 08:48:10 -0700

    finish js game

--- main.js
+++ main.js
@@ -12,44 +12,69 @@
 //   alert("step 2")
 // }
 
-alert("Try to make it to work on time!")
-const runningLate = prompt("You reach a fork in the road. Go left or right?")
-let isLate = false
-if (runningLate === "right") {
-  alert("You chose wrong. Bumper to bumper traffic!")
-  isLate = true
-} else if (isLate == false) {
-  alert("Smooth sailing!")
-}
+// alert("Try to make it to work on time!")
+// const runningLate = prompt("You reach a fork in the road. Go left or right?")
+// let isLate = false
+// if (runningLate === "right") {
+//   alert("You chose wrong. Bumper to bumper traffic!")
+//   let isLate = true
+//   if (isLate === true) {
+//     console.log('not going to run')
+//   }
+// } else {
+//   alert("Smooth sailing!")
+// }
 
-let newChoice
-if (isLate === false) {
-  newChoice = prompt(
-    "Your phone rings. It's your friend. Do you answer or let it go?"
-  )
-}
-if (newChoice === "answer") {
-  alert(
-    "You looked down and didn't notice a cop behind you. He's pulling you over and you're screwed."
-  )
-  isLate = true
-} else {
-  alert("You noticed a cop behind you and ignored the call.")
-}
+// let newChoice
+// if (isLate === false) {
+//   newChoice = prompt(
+//     "Your phone rings. It's your friend. Do you answer or let it go?"
+//   )
+// }
+// if (newChoice === "answer") {
+//   alert(
+//     "You looked down and didn't notice a cop behind you. He's pulling you over and you're screwed."
+//   )
+//   isLate = true
+//   console.log('not going to run')
+// } else {
+//   alert("You noticed a cop behind you and ignored the call.")
+// }
 
-let lastChoice
-if (isLate === false) {
-  lastChoice = prompt(
-    "You're almost there, but you're still half asleep. Get coffee or go tired?"
-  )
-}
-if (lastChoice === "go tired") {
-  alert(
-    "You parked with enough time! But, after parking you shut your eyes for what felt like a blink, and fell asleep. You're fired."
-  )
-  isLate = true
-} else {
-  alert(
-    "There was no line for the coffee. You got lucky, got your coffee and made it to work with a few minutes to spare. Good job!"
-  )
+// let lastChoice
+// if (isLate === false) {
+//   lastChoice = prompt(
+//     "You're almost there, but you're still half asleep. Get coffee or go tired?"
+//   )
+ alert("Try to make it to work on time!")
+ const runningLate = prompt("Skip breakfast?")
+ let yourLate = false
+ if (runningLate === "no") {
+    alert("What were you thinking?")
+    console.log
+    yourLate = true
+ } else {
+   alert("Smart choice!")
+ }
+
+ if (yourLate == false) {
+   alert("what route to take")
+   const routeChoice = prompt("Do you take the freeway or surface streets?")
+   if (routeChoice === "freeway") {
+     alert("You chose wrong. Theres a pile up. You are screwed.")
+     yourLate = true
+   } else {
+     alert("Good choice. There is no traffic and youre almost there.")
+   }
+ }
+
+ if (yourLate == false) {
+  alert("Youre still half asleep!")
+  const routeChoice = prompt("Stop for coffee?")
+  if (routeChoice === "no") {
+    alert("You parked with a few minutes to spare. You close your eyes for what feels like a blink, and fall asleep in the car. Youre fired.")
+    yourLate = true
+  } else {
+    alert("You got lucky. no line at the coffee shop, and you made it just in time, ready to go for the day. You made it!")
+  }
 }
